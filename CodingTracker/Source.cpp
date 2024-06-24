8c22b00357896f8907a7b93ac577ba50b49298e9
Jobson Almeida <jobsonbox@gmail.com>
2024-06-24 15:52:03 -0300

    Revert "Keeping one message.cpp"
    This reverts commit 7fed74b04474b9e4e2efed16e0dc6a841b728100.

--- CodingTracker/Source.cpp
+++ CodingTracker/Source.cpp
@@ -3,7 +3,7 @@
 int main()
 {
 
-	std::cout << "Studying how to work with Git and GitHub through Visual Sutdio - made a modification\n";
+	std::cout << "Studyin how to work with Git and GitHub through Visual Sutdio - made a modification\n";
 	
 	return 0;
 }
