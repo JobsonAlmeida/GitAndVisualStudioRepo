d8f31f34b04998ea6c9998d2f6f5b210b2021d9c
Jobson Almeida <jobsonbox@gmail.com>
2024-06-24 15:35:01 -0300

    Now we have many messages

--- CodingTracker/Source.cpp
+++ CodingTracker/Source.cpp
@@ -4,5 +4,13 @@ int main()
 {
 
 	std::cout << "Studyin how to work with Git and GitHub through Visual Sutdio - made a modification\n";
+
+	std::cout << "Studyin how to work with Git and GitHub through Visual Sutdio - made a modification\n";
+
+	std::cout << "Studyin how to work with Git and GitHub through Visual Sutdio - made a modification\n";
+
+	std::cout << "Studyin how to work with Git and GitHub through Visual Sutdio - made a modification\n";
+
+	std::cout << "Studyin how to work with Git and GitHub through Visual Sutdio - made a modification\n";
 	return 0;
 }
