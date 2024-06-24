2854781aa0a38f6188037b0c754c31fc65d7e33b
Jobson Almeida <jobsonbox@gmail.com>
2024-06-24 16:12:00 -0300

    Initial Commit

--- CodingTracker/Source.cpp
+++ CodingTracker/Source.cpp
@@ -4,6 +4,15 @@ int main()
 {
 
 	std::cout << "Studyin how to work with Git and GitHub through Visual Sutdio - made a modification\n";
+
+
+	std::cout << "A branch of new changes\n";
+
+	int Number{ 10 };
+	float Sum(float number1, float number2)
+	{
+		return number1 + number2; 
+	}
 	
 	return 0;
 }
